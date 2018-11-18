564245dcf0d17fcc1c74bf4c68ef248d4d5dc3ad
Guily <thomas.guily1998@gmail.com>
2018-11-18 17:46:35 +0100

    decode information in messagedAlice

--- DecodeLeMessagedAlice.m
+++ DecodeLeMessagedAlice.m
@@ -7,27 +7,30 @@ addpath ('C:/Inge/avoir/opti/lin');
 % load la matrice d'encodage et le message d'Alice transmis par le canal: 
 load ('messagedAlice.mat'); 
 
-bool = input ('0 = continuous; 1 = integers: entrer le booleen: ');
-if bool < 1
-  fprintf('Vous avez choisi la solution relaxée du problème');
-endif
-if bool > 0
-  fprintf('Vous avez choisi la résolution non continue (les variables seront binaires)');
-endif
+a = size(A);
+n= a(2);
+m = 4*n;
+%valeurs n et m reprises
+bool = 0;
 % Utilisez votre algorithme pour résoudre:
 % 
 %   min_{0 <= xprime <= 1} ||A*xprime - yprime||_2^2 
 %
 % Rem. La solution xprime devrait être un vecteur de taille 98x1
-[xprime,n] = votrealgorithme(A,yprime,bool);
+[xprime] = votrealgorithme(A,yprime,bool,n,m);
  
 
 
-for i= 1:n 
-  xprime (i) = round (xprime(i));
-  xp(i)=xprime(i);
-endfor
+
+
+xprime = resize(xprime, n);
 %virer les ti inutiles
 
+for j= 1 : n 
+    xp(j) = xprime(j);
+    xp(j) = round (xp(j));
+    %arrondir solution obtenue  
+  endfor
+
 % Affichez le résultat: 
-fprintf('The recovered message is: %s \n', decoding_bin(xprime,d)); 
+fprintf('The recovered message is: %s \n', decoding_bin(xp,d)); 
